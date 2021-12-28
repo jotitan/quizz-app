400195a85ed536e3ba41b006b9ba679ee0e97dfe
jonathan.baranzini <titanbar@hotmail.com>
2021-12-28 22:31:59 +0100

    Correct button container width

--- resources/src/components/RoundButton/index.js
+++ resources/src/components/RoundButton/index.js
@@ -3,7 +3,7 @@ import '../../App.css';
 
 export default function RoundButton({title,action}){
 
-    return <div style={{textAlign:'center',position:'absolute',bottom:30,width:'100%'}}>
+    return <div style={{textAlign:'center',position:'absolute',bottom:30,width:'calc(100% - 50px)'}}>
         <div style={{display:'inline-block'}}>
             <div className={"circle-button"}><span onClick={action}>{title}</span></div>
         </div>
